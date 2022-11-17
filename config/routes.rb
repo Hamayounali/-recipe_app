b9d2a234b9c5853813ea61957fc40a289dd674b9
Isigba Ambrose <ambrisexyz@gmail.com>
2022-11-17 11:09:59 +0100

    added styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+end
+
   root to: 'recipes#index'
 
   # get 'recipe_food/quantity:integer'
@@ -9,7 +13,6 @@ Rails.application.routes.draw do
   resources :recipes
   resources :public_recipes
   resources :shopping_list
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
 end
