e94f512b818dca024610ba6539334cdfe26092d7
Hamayoun <humayoonali2@gmail.com>
2022-11-17 15:29:43 +0430

    Updated the navbar

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,35 +1,19 @@
-ShoppingListsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_shopping_list, only: %i[ show edit update destroy ]
-    before_action :set_user, only: %i[ index show new edit create update destroy ]
-    before_action :set_food, only: %i[ index show new edit create update destroy ]
-    before_action :set_recipe, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_recipe, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food_recipe, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food_recipe_food, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food_recipe_recipe, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food_food, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_recipe_recipe, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food_recipe_food_food, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food_recipe_recipe_recipe, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_food_food_food, only: %i[ index show new edit create update destroy ]
-    before_action :set_shopping_list_recipe_recipe_recipe, only: %i[ index show new edit create update destroy ]
+class ShoppingListsController < ApplicationController
+  before_action :authenticate_user!
 
-    # GET /shopping_lists or /shopping_lists.json
-    def index
-        @shopping_lists = ShoppingList.all
-        end
+  # GET /shopping_lists or /shopping_lists.json
+  def index
+    @shopping_lists = ShoppingList.all
+    end
 
-        # GET /shopping_lists/1 or /shopping_lists/1.json
-        def show
-        end
+    # GET /shopping_lists/1 or /shopping_lists/1.json
+    def show
+    end
 
-        # GET /shopping_lists/new
-        def new
-            @shopping_list = ShoppingList.new
-        end
+     # GET /shopping_lists/new
+    def new
+       @shopping_list = ShoppingList.new
+    end
 
-        # GET /shopping_lists/1/edit
-
-        
+    # GET /shopping_lists/1/edit
+    end
